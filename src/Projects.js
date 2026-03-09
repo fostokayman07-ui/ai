038dd342582f9713e4b5573ce3bdaea3650bceaf
fostokayman07-ui <fostokayman07@gmail.com>
2026-03-09 07:51:26 -0400

    sync: full ai-portfolio contents to main

---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <ul>
+        <li>Project 1: Description here</li>
+        <li>Project 2: Description here</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
